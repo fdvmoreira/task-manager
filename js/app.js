917e6d9ba2db049a6b8b37430d65982b6e5379fe
Flavio Moreira <mr.flaviomoreira@gmail.com>
2021-11-08 02:15:17 +0000

    Implement function to get the input from user

--- js/app.js
+++ js/app.js
@@ -6,11 +6,38 @@ const inputTextField = document.querySelector("#inputText");
 const btnAddToList = document.querySelector("#btnAddToList");
 const list = document.querySelector("#taskList")
 
+const form = document.querySelector("#inputForm");
+
+/* * Set event handler for when the form is submitted - press ENTER or click the button */
+form.onsubmit = (event) => {
+
+    event.preventDefault();
+
+    const text = getInputText();
+    inputTextField.value = "";
+    if (text.trim().length > 0) {
+        
+        const item = createNewTask(text.trim());
+        console.log("Item was " + item?.childNodes[0].textContent);
+    }
+    /*
+    let data = saveItem(item);
+    updateUI(data); */
+
+};
+
+// console.log("form");
+
 // get text
 // create new item element
 // save it to local storage
 // update the UI
 
-//const getInputText = ()=> inputTextField.textContent;
+/**
+ * Get the text from input field
+ * @returns text: string
+ */
+const getInputText = () => inputTextField.value.toString();
 
-//console.log();
+
+//console.log(inputTextField.getAttribute('value'));
