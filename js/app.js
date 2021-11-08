b30729f0c3d84249932cd505e863e32b07359f13
Flavio Moreira <mr.flaviomoreira@gmail.com>
2021-11-08 00:33:24 +0000

    Get reference to UI elements

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+/** Task Manager */
+
+const container = document.querySelector('.container');
+const inputTextField = document.querySelector("#inputText");
+const btnAddToList = document.querySelector("#btnAddToList");
+const list = document.querySelector("#taskList")
+
+// get text
+// create new item element
+// save it to local storage
+// update the UI
+
+//const getInputText = ()=> inputTextField.textContent;
+
+//console.log();
