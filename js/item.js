881f25273a1b95c703d0020588c6a8d6c37708e9
Flavio Moreira <mr.flaviomoreira@gmail.com>
2021-11-26 23:58:53 +0000

    Implement Item class to represent each list item

---
+++ js/item.js
@@ -0,0 +1,16 @@
+class Item {
+    constructor(id, item, checked) {
+        this.#_id = id;
+        this.#_item = item;
+        this.#checked = checked;
+    }
+
+    set id(id) { this.#_id = id; }
+    get id() { return this.#_id; }
+
+    set item(item) { this.#_item = item; }
+    get item() { return this.#_item; }
+
+    set checked(checked) { this.#_checked = checked; }
+    get checked() { return this.#_checked; }
+}
