6dceda7ea1af475beb99428b33c03e82b496d7bf
Flavio Moreira <mr.flaviomoreira@gmail.com>
2021-11-28 02:24:46 +0000

    Complete the functionalities of marking and unmarking items and save the state

--- js/app.js
+++ js/app.js
@@ -12,11 +12,10 @@
 //[*] Implement clickable task item
 //[*] delete item from the list
 //[*] @fixup - When reloading the page if items are saved the first added will replace the intem at index 0
-//[] @fix - remove the setTimeOut function and find another implementation (Promise) to complete a task before moving the next one
 //[] Save the state of complete tasks
-//[] Move/archive completed tasks
 //[] implement removal and completion notification
-
+//[] @fix - remove the setTimeOut function and find another implementation (Promise) to complete a task before moving the next one
+//[] Move/archive completed tasks
 
 import Item from "./item.js";
 
@@ -38,18 +37,18 @@ window.onload = (env) => {
     }
 }
 
-
-
-/* * Set event handler for when the form is submitted - press ENTER or click the button */
-form.onsubmit = (event) => {
-
+/* *
+* Set event handler for when the form is submitted - press ENTER or click the button 
+*/
+form.onsubmit = () => {
     const text = getInputText();
     inputTextField.value = "";
     if (text.trim().length > 0) {
         const item = createNewTask(text.trim(SEPARATOR));
 
+        const task = new Item(tasks.length, item.childNodes[1].textContent, false)
         // Pass index and task, so that we have the keys to retrieve item from storage
-        let itemWasSaved = saveItem(tasks.length + SEPARATOR + item.childNodes[1].textContent);
+        let itemWasSaved = saveItem(task);
 
         if (!itemWasSaved) {
             console.error("Your item was not saved to local storage.");
@@ -63,20 +62,16 @@ form.onsubmit = (event) => {
 
         // Update the UI
         updateUI();
-
-
     }
 }
 
 /**
- * 
  * Get the text from input field
  * @returns text: string
  */
 const getInputText = () => inputTextField.value.toString();
 
 /**
- * 
  * Create new item
  * @param text - text string to be added to the list 
  * @returns newly created item
@@ -88,23 +83,19 @@ const createNewTask = (text) => {
     const deleteBtn = document.createElement("span");
     deleteBtn.className = "delete-btn";
     deleteBtn.innerHTML = "delete";
-    // span.setAttribute("class", "checkbox");
     li.appendChild(checkbox);
-    li.appendChild(document.createTextNode(" " + text));
+    li.appendChild(document.createTextNode("" + text));
     li.appendChild(deleteBtn);
     return li;
 }
 
 /**
- * 
- * @param {String} item - the text to be added to local storage, format "[index]-[text]" 
+ * @param {Item} item - the object to be added to local storage, format "[index]-[text]" 
  * @returns Boolean True if the item added to storage and false otherwise
  */
-const saveItem = (item) => {
-    let [key, ...value] = item.split(SEPARATOR);
-
+const saveItem = (itemObj) => {
     try {
-        localStorage.setItem(key, value);
+        localStorage[itemObj.id] = JSON.stringify(itemObj);
     } catch (error) {
         console.error(error);
         return false;
@@ -113,19 +104,24 @@ const saveItem = (item) => {
 }
 
 /**
- * 
  * Load the data from storage and fill the tasks array
  * @returns the items stored in local storage
  */
 const loadItems = () => {
     let items = [];
-
     for (let i = 0; i < localStorage.length; i++) {
-        items.push(localStorage.getItem(i.toString(10)));
+        items.push(localStorage.getItem(i.toString()));
     }
-
     // flatten the array to remove empty items
-    return items.flat();
+    items = items.flat();
+    items.forEach((value, index) => {
+        const newItem = JSON.parse(value);
+        newItem._id = index;
+        items[index] = JSON.stringify(newItem);
+        console.log("Loading ... " + items[index]); // Remove me
+    });
+
+    return items;
 }
 
 
@@ -136,11 +132,17 @@ const updateUI = () => {
 
     // add new elements
     if (tasks.length !== 0) {
-        tasks.forEach((value) => {
-            unorderedListElement.appendChild(createNewTask(value));
+        tasks.forEach((value, index) => {
+            const task = JSON.parse(value);
+            const newItem = createNewTask(task._item);
+
+            if (task._checked) {
+                markItemAsComplete(newItem, "green");
+            }
+            unorderedListElement.appendChild(newItem);
         });
-        addClickEventBehaviourToListItems();
     }
+    addEventHandlerToButtons();
 }
 
 /**
@@ -157,42 +159,22 @@ const cleanupUI = () => {
 }
 
 /**
- * Add click event to item to mark them as complete or not
- */
-const addClickEventBehaviourToListItems = () => {
-    let listOfItems = document.querySelectorAll("li");
-    listOfItems.forEach((listItem, index) => {
-        const checkbox = listItem.childNodes[INDEX_CHECKBOX];
-        const deleteBtn = listItem.childNodes[INDEX_DELETE_ITEM];
-        // mark item as complete
-        checkbox.addEventListener("click", () => {
-            let bgcolor = listItem.childNodes[INDEX_CHECKBOX].style.backgroundColor;
-            markItemAsComplete(listItem.childNodes[INDEX_CHECKBOX], bgcolor);
-
-        });
-        // delete item from list
-        deleteBtn.addEventListener("click", () => {
-            let elIndex = Array.from(deleteBtn.parentElement.parentElement.children).indexOf(deleteBtn.parentElement);
-            console.log("li index " + elIndex);
-            deleteItemFromList(elIndex);
-        });
-    });
-}
-
-/**
  * Change the appearence of the checkbox and text to simulate a complete task
  * @param {HTMLElement} element - the element to change get the background color changed
  * @param {String} bgcolor - the color to apply to element's background
  */
 const markItemAsComplete = (element, bgcolor) => {
-    element.style.backgroundColor = ((bgcolor.length > 0) ? (() => {
-        element.parentElement.className = "";
-        return "";
-    })() : (() => {
-        element.parentElement.className = "strike-through";
-        return "green";
-    })());
+    element.childNodes[0].style.backgroundColor = bgcolor;
+    element.className = "strike-through";
+}
 
+/**
+ * Unmark the item from the list
+ * @param {HTMLElement} element the element to be unmarked
+ */
+const unmarkListItem = (element) => {
+    element.childNodes[0].style.backgroundColor = "";
+    element.className = "";
 }
 
 /**
@@ -201,17 +183,72 @@ const markItemAsComplete = (element, bgcolor) => {
  * @returns void
  */
 const deleteItemFromList = (index) => {
-    const deleteItem = tasks.splice(index, 1);
-    if (deleteItem.length === 0) {
+    const deletedItem = tasks.splice(index, 1);
+    if (deletedItem.length === 0) {
         console.error("index:" + index + " Error: Item was not removed!");
         return;
     }
     console.log("index:" + index + " Item removed from list.");
+    // TODO delete single item from storage and
     tasks = tasks.flat();
     localStorage.clear();
     tasks.forEach((task, id) => {
-        localStorage.setItem(id.toString(), task);
+        let item = JSON.parse(task);
+        saveItem(new Item(id, item._item, item._checked));
+        // localStorage.setItem(id.toString(), task);
     });
     cleanupUI();
     updateUI();
+}
+
+/**
+ * update the status of item
+ * @param {Number} index of the array 
+ * @param {Boolean} done if an item is complete or not
+ */
+const updateItemStatus = (index, done) => {
+    tasks = tasks.flat();
+    console.log("index: " + index + " task " + tasks[index]);
+    let newItem = JSON.parse(tasks[index]);
+    newItem._checked = done;
+    tasks[index] = JSON.stringify(newItem); // update array
+    saveItem(new Item(index, newItem._item, newItem._checked)); //update localstorage
+}
+
+/**
+ * Checks if an item has been marked as complete
+ * @param {HTMLElement} listItem list item to check
+ * @returns true or false
+ */
+const isChecked = (element) => element.childNodes[INDEX_CHECKBOX].style.backgroundColor.length > 0;
+
+/**
+ * Add the event handler to the complete and delete buttons
+ */
+const addEventHandlerToButtons = () => {
+    let listItems = document.querySelector("#taskList").children;
+    console.log(listItems);
+    for (let newItem of listItems) {
+        const checkbox = newItem.childNodes[INDEX_CHECKBOX];
+        const deleteBtn = newItem.childNodes[INDEX_DELETE_ITEM];
+
+        // check the box
+        checkbox.addEventListener("click", () => {
+            let index = Array.from(checkbox.parentElement.parentElement.children).indexOf(checkbox.parentElement);
+            if (!isChecked(newItem)) {
+                updateItemStatus(index, true);
+                markItemAsComplete(newItem, "green");
+                return
+            }
+            updateItemStatus(index, false);
+            unmarkListItem(newItem);
+        });
+
+        // delete item from list
+        deleteBtn.addEventListener("click", () => {
+            let elIndex = Array.from(deleteBtn.parentElement.parentElement.children).indexOf(deleteBtn.parentElement);
+            console.log("li index " + elIndex);
+            deleteItemFromList(elIndex);
+        });
+    }
 }
--- js/item.js
+++ js/item.js
@@ -1,19 +1,16 @@
 export default class Item {
-    #_id;
-    #_item;
-    #_checked;
     constructor(id, item, checked) {
-        this.#_id = id;
-        this.#_item = item;
-        this.#_checked = checked;
+        this._id = id;
+        this._item = item;
+        this._checked = checked;
     }
 
-    set id(id) { this.#_id = id; }
-    get id() { return this.#_id; }
+    set id(id) { this._id = id; }
+    get id() { return this._id; }
 
-    set item(item) { this.#_item = item; }
-    get item() { return this.#_item; }
+    set item(item) { this._item = item; }
+    get item() { return this._item; }
 
-    set checked(checked) { this.#_checked = checked; }
-    get checked() { return this.#_checked; }
+    set checked(checked) { this._checked = checked; }
+    get checked() { return this._checked; }
 }
