9964de08190fbe53dd8c059988101280913f1586
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-11-01 22:25:05 +0000

    add test for toggleBoolString function

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -2,7 +2,7 @@
 
 import {describe, test, expect} from "@jest/globals";
 
-import {InvalidItemInstanceObjectError, createListItemElement} from "../js/app";
+import {InvalidItemInstanceObjectError, createListItemElement, toggleBoolString} from "../js/app";
 import Item from "../js/item";
 
 /**
@@ -31,4 +31,29 @@ describe("createListItemElement", ()=>{
 		expect(li.getAttribute("data-checked")).toBe("false");
 		expect(li.textContent).toBe("New Item");
 	});
+});
+
+/**
+	* @group Utils
+	*/ 
+describe("toggleBooleanString", ()=>{
+	/**
+		* @test {Utils}
+		*/ 
+	test("should return true", ()=>{
+		expect(toggleBoolString("false")).toEqual("true");
+	});
+		/**
+		* @test {Utils}
+		*/
+	test("should return false", ()=>{
+		expect(toggleBoolString("true")).toEqual("false");
+	});
+		/**
+		* @test {Utils}
+		*/
+	test("should return false", ()=>{
+		expect(toggleBoolString("")).toEqual("false");
+	});
+
 });
