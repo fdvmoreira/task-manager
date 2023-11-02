2c61b3d638bce4c0246dfbf832bad4158c4c6e81
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-11-02 12:56:50 +0000

    Format the file with prettier

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,59 +1,65 @@
+
 /** @jest-environment jsdom */
 
 import { describe, test, expect } from "@jest/globals";
 
-import { InvalidItemInstanceObjectError, createListItemElement, toggleBoolString } from "../js/app";
+import {
+  InvalidItemInstanceObjectError,
+  createListItemElement,
+  toggleBoolString,
+} from "../js/app";
+
 import Item from "../js/item";
 
 /**
  * @group Utils
  */
 describe("createListItemElement", () => {
+  /**
+   * @test {Utils}
+   */
+  test("shoul throw an InvalidItemInstanceObjectError exception", () => {
+    /** @type {Item} */
+    const item = new Item(9, "hello", null);
+    expect(() => createListItemElement(item)).toThrowError(
+      InvalidItemInstanceObjectError,
+    );
+  });
 
-	/**
-	* @test {Utils}
-	*/
-	test("shoul throw an InvalidItemInstanceObjectError exception", () => {
-		/** @type {Item} */
-		const item = new Item(9, "hello", null);
-		expect(() => createListItemElement(item)).toThrowError(InvalidItemInstanceObjectError);
-	});
-
-	/**
-	* @test {Utils}
-	*/
-	test("should create an li element", () => {
-		/** @type {Item} */
-		const item = new Item(999, "New Item", false);
-		/** @type {?HTMLLIElement} */
-		const li = createListItemElement(item);
-		expect(li.getAttribute("data-id")).toBe("999");
-		expect(li.getAttribute("data-checked")).toBe("false");
-		expect(li.textContent).toBe("New Item");
-	});
+  /**
+   * @test {Utils}
+   */
+  test("should create an li element", () => {
+    /** @type {Item} */
+    const item = new Item(999, "New Item", false);
+    /** @type {?HTMLLIElement} */
+    const li = createListItemElement(item);
+    expect(li.getAttribute("data-id")).toBe("999");
+    expect(li.getAttribute("data-checked")).toBe("false");
+    expect(li.textContent).toBe("New Item");
+  });
 });
 
 /**
-	* @group Utils
-	*/
+ * @group Utils
+ */
 describe("toggleBooleanString", () => {
-	/**
-		* @test {Utils}
-		*/
-	test("should return true", () => {
-		expect(toggleBoolString("false")).toEqual("true");
-	});
-	/**
-	* @test {Utils}
-	*/
-	test("should return false", () => {
-		expect(toggleBoolString("true")).toEqual("false");
-	});
-	/**
-	* @test {Utils}
-	*/
-	test("should return false", () => {
-		expect(toggleBoolString("")).toEqual("false");
-	});
-
+  /**
+   * @test {Utils}
+   */
+  test("should return true", () => {
+    expect(toggleBoolString("false")).toEqual("true");
+  });
+  /**
+   * @test {Utils}
+   */
+  test("should return false", () => {
+    expect(toggleBoolString("true")).toEqual("false");
+  });
+  /**
+   * @test {Utils}
+   */
+  test("should return false", () => {
+    expect(toggleBoolString("")).toEqual("false");
+  });
 });
