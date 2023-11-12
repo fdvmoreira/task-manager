0a85f65a1330d58a17cf3206711beda072cacb77
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-11-12 17:34:15 +0000

    Format and add test for updating the state of an Item in the UI

--- tests/writeToDocument.test.js
+++ tests/writeToDocument.test.js
@@ -5,12 +5,19 @@ import { beforeAll, afterEach, expect, describe, test } from "@jest/globals";
 
 import { JSDOM } from "jsdom";
 
-import { clearList, createListItemElement, insertItemToList } from "../js/app";
+import {
+  clearList,
+  createListItemElement,
+  insertItemToList,
+  storage,
+  unorderedListElement,
+  updateListItemState,
+} from "../js/app";
 import Item from "../js/item";
 
 const html = String.raw`
-	<ul data-id="list">
-		<li> First element </li>
+	<ul data-id="task-list">
+		<li data-checked="false" data-id="999"> First element </li>
 		<li> Second element </li>
 		<li> Third element </li>
 		<li> Fourth element </li>
@@ -51,7 +58,7 @@ describe("insertItemToList", () => {
     /** @type {Item} */
     const item = new Item(111, "Hello There", true);
     /** @type {HTMLUListElement} */
-    const uList = dom.window.document.querySelector("[data-id='list']");
+    const uList = dom.window.document.querySelector("[data-id='task-list']");
     if (!uList) return Promise.reject("Could not find element");
     /** @type {number} */
     const cnt = uList.children.length;
@@ -59,5 +66,23 @@ describe("insertItemToList", () => {
     // THeSe LINEs WILL NOT WORK. JSDOM does not recognise li created with document.createElement as a type of Node
     // insertItemToList(item, uList);
     // expect(uList.children.length).toEqual(cnt+1);
+  });
+});
+
+/**
+ * Update the state of the item
+ * @group UpdateList
+ */
+describe("updateListItemState", () => {
+  /**
+   * @test {UpdateList}
+   */
+  test("should update the state of an LI element", async () => {
+    /** @type {HTMLLIElement|null} */
+    // const li = dom?.window?.document?.querySelector("[data-id='999']");
+    // if (!li) return Promise.reject("Could Not find LI");
+    // li?.addEventListener("click", updateListItemState);
+    // li?.click();
+    // expect(li?.getAttribute("data-id")).toBe("true");
   });
 });
